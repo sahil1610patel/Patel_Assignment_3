7a33b0b989fdab41a1df46a97450a4978d57a3fb
sahil1610patel <sahil1610patel@gmail.com>
2025-03-06 22:17:55 -0500

    first

---
+++ Assignment 3/tests/test_area.py
@@ -0,0 +1,17 @@
+import pytest  
+from src.area import calculate_area_square  
+  
+def test_calculate_area_square_negative():  
+    with pytest.raises(TypeError):  
+        calculate_area_square(-2)  
+  
+def test_calculate_area_square_string():  
+    with pytest.raises(TypeError):  
+        calculate_area_square("2")  
+  
+def test_calculate_area_square_list():  
+    with pytest.raises(TypeError):  
+        calculate_area_square([2])
+
+def test_calculate_area_square_valid():
+    assert calculate_area_square(27) == 1000
